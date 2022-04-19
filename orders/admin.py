7fa79411930d996f63738aee13e77763b2bf774e
anon <anon@debian-BULLSEYE-live-builder-AMD64>
2022-04-19 13:08:36 +0300

    bugfix

--- orders/admin.py
+++ orders/admin.py
@@ -3,49 +3,49 @@ from django.contrib import admin
 from orders.models import *
 
 
-# class OrderedProductInline(admin.TabularInline):
-#     model = OrderedProductModel
-#     readonly_fields = ('vcode', 'name', 'product_id', 'quantity',)
-#     fieldsets = (
-#         (None, {'fields': ('vcode', 'product_id', 'name', 'price', 'quantity',)}),
-#         )
-#     extra = 0
+class OrderedProductInline(admin.TabularInline):
+    model = OrderedProductModel
+    readonly_fields = ('vcode', 'name', 'product_id', 'quantity',)
+    fieldsets = (
+        (None, {'fields': ('vcode', 'product_id', 'name', 'price', 'quantity',)}),
+        )
+    extra = 0
 
 
-# class CustomerAdmin(admin.ModelAdmin):
-#     inlines = ( OrderedProductInline, )
+class CustomerAdmin(admin.ModelAdmin):
+    inlines = ( OrderedProductInline, )
 
-#     def send_payment_mail(self, obj):
-#         return mark_safe(
-#             '''
-#             <a style="background-color: #73ff54; padding: 7px; color: #3b3b3b; border-radius: 3px;" href="/o/sent_payment_email/%s/">
-#             <b>📩 ОТПРАВИТЬ ПИСЬМО НА ОПЛАТУ</b></a>
-#             '''
-#             % (obj.uuid))
+    def send_payment_mail(self, obj):
+        return mark_safe(
+            '''
+            <a style="background-color: #73ff54; padding: 7px; color: #3b3b3b; border-radius: 3px;" href="/o/sent_payment_email/%s/">
+            <b>📩 ОТПРАВИТЬ ПИСЬМО НА ОПЛАТУ</b></a>
+            '''
+            % (obj.uuid))
 
-#     send_payment_mail.short_description = 'Действия'
+    send_payment_mail.short_description = 'Действия'
 
         
-#     list_display = ('online_pay', 'order_number', 'phone', 'date_created', 'status')
-#     list_display_links = ('online_pay', 'order_number', 'phone', 'date_created',)
-#     list_editable = ('status', )
-#     readonly_fields = (
-#         'order_number', 'date_created', 'adress', 'delivery_adress',
-#         'person', 'phone', 'email', 'comment', 'delivery', 'send_payment_mail',
-#         'company', 'legaladress', 'inn', 'kpp', 'okpo', 'bankname',
-#         'currentacc', 'corresponding', 'bic', 'online_pay', 'payment_uuid', 'uuid',
-#         )
-#     fieldsets = (
-#         ("Заказ", {'fields': ( 'uuid', ('status', 'date_created'), ('order_number', 'position_total', 'total',), ('send_payment_mail', 'per_online_pay',), 'seller_comm', ('online_pay', 'payment_uuid',),'adress')}),
-#         ("Доставка", {'fields': ( ('delivery', 'delivery_summ',), 'delivery_adress',)}),
-#         ("Физическое лицо", {'fields': (('person', 'phone', 'email'), 'comment',)}),
-#         ("Юридическое лицо", {'fields': (
-#             ('company', 'legaladress'),
-#             ('inn', 'kpp',),
-#             ('okpo', 'bankname',),
-#             ('currentacc', 'corresponding', ), 'bic',
-#             )}),
-#         )
+    list_display = ('online_pay', 'order_number', 'phone', 'date_created', 'status')
+    list_display_links = ('online_pay', 'order_number', 'phone', 'date_created',)
+    list_editable = ('status', )
+    readonly_fields = (
+        'order_number', 'date_created', 'adress', 'delivery_adress',
+        'person', 'phone', 'email', 'comment', 'delivery', 'send_payment_mail',
+        'company', 'legaladress', 'inn', 'kpp', 'okpo', 'bankname',
+        'currentacc', 'corresponding', 'bic', 'online_pay', 'payment_uuid', 'uuid',
+        )
+    fieldsets = (
+        ("Заказ", {'fields': ( 'uuid', ('status', 'date_created'), ('order_number', 'position_total', 'total',), ('send_payment_mail', 'per_online_pay',), 'seller_comm', ('online_pay', 'payment_uuid',),'adress')}),
+        ("Доставка", {'fields': ( ('delivery', 'delivery_summ',), 'delivery_adress',)}),
+        ("Физическое лицо", {'fields': (('person', 'phone', 'email'), 'comment',)}),
+        ("Юридическое лицо", {'fields': (
+            ('company', 'legaladress'),
+            ('inn', 'kpp',),
+            ('okpo', 'bankname',),
+            ('currentacc', 'corresponding', ), 'bic',
+            )}),
+        )
 
 
 
@@ -95,12 +95,12 @@ class RequestPriceAdmin(admin.ModelAdmin):
     readonly_fields = ('id', 'city', 'contact', 'product')
 
 
-# admin.site.register(
-#     CustomerModel, 
-    # CustomerAdmin
-    # )
-# admin.site.register(
-#     RequestPriceModel, 
-    # RequestPriceAdmin
-#    )
+admin.site.register(
+    CustomerModel, 
+    CustomerAdmin
+    )
+admin.site.register(
+    RequestPriceModel, 
+    RequestPriceAdmin
+    )
 # admin.site.register(ClientModel, ClientAdmin)
--- orders/views.py
+++ orders/views.py
@@ -32,256 +32,256 @@ from sber.views import SberInterface
 from dreamkas.views import DreamkasInterface
 
 
-# class OrderInfoView(APIView):
-#     """ Информация о заказе """
+class OrderInfoView(APIView):
+    """ Информация о заказе """
     
-#     def get(self, request, order):
-#         # order_number = request.data["orderNumber"]
-#         order = CustomerModel.objects.get(order_number=order.upper())
-#         serializer = SmOrderListSerializer(order, context={'request':request})
-#         if order:
-#             return Response(serializer.data)
-#         else:
-#             return Response({ "error": "Заказ не найден" })
+    def get(self, request, order):
+        # order_number = request.data["orderNumber"]
+        order = CustomerModel.objects.get(order_number=order.upper())
+        serializer = SmOrderListSerializer(order, context={'request':request})
+        if order:
+            return Response(serializer.data)
+        else:
+            return Response({ "error": "Заказ не найден" })
 
 
-# class RegisterPaymentView(APIView):
-#     """ Регистрация оплаты заказа """
+class RegisterPaymentView(APIView):
+    """ Регистрация оплаты заказа """
     
-#     def post(self, request):
-#         order_number = request.data["orderNumber"]
+    def post(self, request):
+        order_number = request.data["orderNumber"]
 
-#         order = CustomerModel.objects.get(order_number=order_number.upper())
-#         if order.per_online_pay:
-#             register_data = SberInterface.payment_register(amount=order.total, order_number=order.order_number)
-#             print(register_data)
-#             try:
-#                 if 'errorCode' in register_data.keys():
-#                     return Response({ "error": "Заказ с таким номером уже обработан" })
-#                 else:
-#                     order_id = register_data["orderId"]
-#                     CustomerModel.objects.filter(order_number=order_number).update(payment_uuid=order_id)
+        order = CustomerModel.objects.get(order_number=order_number.upper())
+        if order.per_online_pay:
+            register_data = SberInterface.payment_register(amount=order.total, order_number=order.order_number)
+            print(register_data)
+            try:
+                if 'errorCode' in register_data.keys():
+                    return Response({ "error": "Заказ с таким номером уже обработан" })
+                else:
+                    order_id = register_data["orderId"]
+                    CustomerModel.objects.filter(order_number=order_number).update(payment_uuid=order_id)
 
-#             except KeyError:
-#                 return Response({"error": "Сервис оплаты временно не доступен"})
+            except KeyError:
+                return Response({"error": "Сервис оплаты временно не доступен"})
 
-#             return Response(register_data)
-#         else:
-#             return Response({"error": "Оплата онлайн будет доступна после проверки заказа"})
+            return Response(register_data)
+        else:
+            return Response({"error": "Оплата онлайн будет доступна после проверки заказа"})
         
 
-# class CheckOrderPaymentView(APIView):
-#     """" Проверка оплаты заказа """
-#     serializer_class = CustomerSerializer
+class CheckOrderPaymentView(APIView):
+    """" Проверка оплаты заказа """
+    serializer_class = CustomerSerializer
 
-#     def post(self, request):
-#         try:
-#             # Проверка оплаты заказа
-#             order_id = request.data["orderId"]
-#             payment_data = SberInterface.payment_data(order_id)
-#             paymentAmountInfo = payment_data["paymentAmountInfo"]
-#             if paymentAmountInfo["paymentState"] == "DEPOSITED":
+    def post(self, request):
+        try:
+            # Проверка оплаты заказа
+            order_id = request.data["orderId"]
+            payment_data = SberInterface.payment_data(order_id)
+            paymentAmountInfo = payment_data["paymentAmountInfo"]
+            if paymentAmountInfo["paymentState"] == "DEPOSITED":
 
-#                 # Отправляем на фискализацию и ставим статус оплаты в заказе
-#                 order = CustomerModel.objects.filter(payment_uuid=order_id)
-#                 serializer = QOrderListSerializer(order[0], context={'request':request})
-#                 if not order[0].online_pay:
-#                     order.update(online_pay=True)
-#                     receipt_data = DreamkasInterface.generate_receipt(serializer.data)
-#                     receipt_result = DreamkasInterface.receipting_dreamkas(receipt_data)
+                # Отправляем на фискализацию и ставим статус оплаты в заказе
+                order = CustomerModel.objects.filter(payment_uuid=order_id)
+                serializer = QOrderListSerializer(order[0], context={'request':request})
+                if not order[0].online_pay:
+                    order.update(online_pay=True)
+                    receipt_data = DreamkasInterface.generate_receipt(serializer.data)
+                    receipt_result = DreamkasInterface.receipting_dreamkas(receipt_data)
 
-#                     # Отправляем уведомлнеие в агент
-#                     payment = { "order_number": order[0].order_number, "total": order[0].total, "payment_uuid": order_id }
-#                     # send_alert_to_agent(payment=payment)
+                    # Отправляем уведомлнеие в агент
+                    payment = { "order_number": order[0].order_number, "total": order[0].total, "payment_uuid": order_id }
+                    # send_alert_to_agent(payment=payment)
                     
-#                 else:
-#                     order.update(online_pay=False)
+                else:
+                    order.update(online_pay=False)
 
 
-#             return Response(payment_data)
-#         except KeyError:
-#             return Response({ "error": "Не были предоставлены данные" })
+            return Response(payment_data)
+        except KeyError:
+            return Response({ "error": "Не были предоставлены данные" })
 
 
-# """ Изменение статуса заказа отработанный или не отработанный """
-# def edit_order_status(request, uuid, status):
-#     post = "Заказ успешно помечен"
-#     qs = CustomerModel.objects.filter(uuid=uuid).update(status=status)
-#     if qs == 0:
-#         post = "Заказ не найден в системе"
-#     return render(request, 'questionclosed.html', { 'post': post})
+""" Изменение статуса заказа отработанный или не отработанный """
+def edit_order_status(request, uuid, status):
+    post = "Заказ успешно помечен"
+    qs = CustomerModel.objects.filter(uuid=uuid).update(status=status)
+    if qs == 0:
+        post = "Заказ не найден в системе"
+    return render(request, 'questionclosed.html', { 'post': post})
 
 
-# class OrderListViews(APIView):
-#     """ Заказы пользователя """
-#     serializer_class = OrderListSerializer
-#     permission_classes = [IsAuthenticated,]
+class OrderListViews(APIView):
+    """ Заказы пользователя """
+    serializer_class = OrderListSerializer
+    permission_classes = [IsAuthenticated,]
 
-#     def get(self, request):
-#         user = request.user
+    def get(self, request):
+        user = request.user
         
-#         orders = CustomerModel.objects.filter(email=user.email)
-#         # Добавить в QS фильтер по телефону или другую мех. для сбора ВСЕХ заказов пользователя
-#         # all_order = CustomerModel.objects.filter(=user.email)
-#         # print(all_order)
+        orders = CustomerModel.objects.filter(email=user.email)
+        # Добавить в QS фильтер по телефону или другую мех. для сбора ВСЕХ заказов пользователя
+        # all_order = CustomerModel.objects.filter(=user.email)
+        # print(all_order)
 
-#         serializer = self.serializer_class(orders, many=True, context={"request": request})
-#         return Response(serializer.data)
+        serializer = self.serializer_class(orders, many=True, context={"request": request})
+        return Response(serializer.data)
 
 
 
-# from serializers.catalog import ListProductsSerializer
-# class OrderViews(APIView):
-#     """ Обработка заказов """
-#     serializer_class = CustomerSerializer
+from serializers.catalog import ListProductsSerializer
+class OrderViews(APIView):
+    """ Обработка заказов """
+    serializer_class = CustomerSerializer
 
-#     def post(self, request, format=None):
-#         """ Извлечение данных и структурирование заказа """
-#         data=request.data
-#         region_code = data.pop('region_code')
-#         select_shop = data.pop('shop_id')
+    def post(self, request, format=None):
+        """ Извлечение данных и структурирование заказа """
+        data=request.data
+        region_code = data.pop('region_code')
+        select_shop = data.pop('shop_id')
 
-#         # Расчитываем стоимость заказа из товаров в БД (security)
-#         products = []
-#         for product in data['client_product']:
-#             products.append(product['id'])
+        # Расчитываем стоимость заказа из товаров в БД (security)
+        products = []
+        for product in data['client_product']:
+            products.append(product['id'])
 
-#         products_qs = ProductModel.objects.filter(id__in=products)
-#         products_serializer = ListProductsSerializer(products_qs, many=True, context={'request': request})
+        products_qs = ProductModel.objects.filter(id__in=products)
+        products_serializer = ListProductsSerializer(products_qs, many=True, context={'request': request})
         
-#         actual_price = {}
-#         for product in products_serializer.data:
-#             for price in dict(product)['prod_price']:
-#                 if select_shop == price['shop']:
-#                     actual_price[product['id']] = int(price['price'])
-#                     break
+        actual_price = {}
+        for product in products_serializer.data:
+            for price in dict(product)['prod_price']:
+                if select_shop == price['shop']:
+                    actual_price[product['id']] = int(price['price'])
+                    break
 
-#         # Закидываем с ключём price в объект data
-#         for client_product in data['client_product']:
-#             client_product['price'] = actual_price[client_product['id']]
-#             client_product['product_id'] = int(client_product['id'])
+        # Закидываем с ключём price в объект data
+        for client_product in data['client_product']:
+            client_product['price'] = actual_price[client_product['id']]
+            client_product['product_id'] = int(client_product['id'])
 
-#         # Присвоение кода и вычисление суммы по позициям заказа
-#         data['order_number'] = region_code + str(random.randrange(1000000, 1999999))
-#         data['position_total'] = get_position_summ(data['client_product'])
+        # Присвоение кода и вычисление суммы по позициям заказа
+        data['order_number'] = region_code + str(random.randrange(1000000, 1999999))
+        data['position_total'] = get_position_summ(data['client_product'])
 
-#         # Вычисление итога заказа
-#         if data['delivery']:
-#             data['total'] = data['position_total'] + data['delivery_summ']
-#         else:
-#             data['total'] = data['position_total']
+        # Вычисление итога заказа
+        if data['delivery']:
+            data['total'] = data['position_total'] + data['delivery_summ']
+        else:
+            data['total'] = data['position_total']
 
-#         # Создание заказа в БД
-#         serializer = self.serializer_class(data=data)
-#         if serializer.is_valid():
-#             serializer.save()
+        # Создание заказа в БД
+        serializer = self.serializer_class(data=data)
+        if serializer.is_valid():
+            serializer.save()
 
-#             # Логика оповещений
-#             send_alert_to_agent(order=serializer.data)
-#             mail_list = [
-#                 'shop@glsvar.ru',
-#                 ]
+            # Логика оповещений
+            send_alert_to_agent(order=serializer.data)
+            mail_list = [
+                'shop@glsvar.ru',
+                ]
 
-#             if serializer.data['email']:
-#                 mail_list.append(serializer.data['email'])
+            if serializer.data['email']:
+                mail_list.append(serializer.data['email'])
 
-#             OrderMails.send_notice(email=mail_list, data=serializer.data)
-#             return Response({ 'order': data['order_number'] })
-#         else:
-#             return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            OrderMails.send_notice(email=mail_list, data=serializer.data)
+            return Response({ 'order': data['order_number'] })
+        else:
+            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
-# class OrderStatusViews(APIView):
-#     """ Запрос статуса заказа """
+class OrderStatusViews(APIView):
+    """ Запрос статуса заказа """
 
-#     def post(self, request):
-#         code = request.data['code'] if request.data['code'] else None
+    def post(self, request):
+        code = request.data['code'] if request.data['code'] else None
 
-#         if code and 4 < len(code) < 10:
-#             order_qs = CustomerModel.objects.filter(order_number=code)
+        if code and 4 < len(code) < 10:
+            order_qs = CustomerModel.objects.filter(order_number=code)
             
-#             try:
-#                 status_name = order_qs[0].status
-#             except IndexError:
-#                 return Response(status=status.HTTP_400_BAD_REQUEST)
+            try:
+                status_name = order_qs[0].status
+            except IndexError:
+                return Response(status=status.HTTP_400_BAD_REQUEST)
 
-#             return Response({"status": status_name })
+            return Response({"status": status_name })
 
-#         return Response(status=status.HTTP_400_BAD_REQUEST)
-
-
-# class RequestPriceViews(APIView):
-#     """ Запрос цены на товар """
-
-#     def post(self, request):
-#         data = request.data
-#         serializer = RequestPriceSerializer(data=data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             send_alert_to_agent(pricerequest=serializer.data)
-#             return Response(serializer.data)
-
-#         return Response(status=status.HTTP_400_BAD_REQUEST)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
+class RequestPriceViews(APIView):
+    """ Запрос цены на товар """
 
-# class MailsView(APIView):
-#     """ """
-#     def get(self, request):
-#         mail = False
-#         count = 0
-#         if mail:
-#             html_content = render_to_string('neuesJahr.html',)
+    def post(self, request):
+        data = request.data
+        serializer = RequestPriceSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            send_alert_to_agent(pricerequest=serializer.data)
+            return Response(serializer.data)
 
-#             with open('list.txt') as file:
-#                 mail_adress = file.read().lower().splitlines()
+        return Response(status=status.HTTP_400_BAD_REQUEST)
 
-#             for mail in mail_adress:
+
+
+class MailsView(APIView):
+    """ """
+    def get(self, request):
+        mail = False
+        count = 0
+        if mail:
+            html_content = render_to_string('neuesJahr.html',)
+
+            with open('list.txt') as file:
+                mail_adress = file.read().lower().splitlines()
+
+            for mail in mail_adress:
                 
-#                 count += 1
-#                 send_mail(
-#                     'ООО Техносвар КС поздравляет Вас с новым годом!',
-#                     message=html_content,
-#                     from_email= 'market1@tehnosvar.ru',
-#                     recipient_list= [f'{mail}',],
-#                     fail_silently=False,
-#                     html_message=html_content
-#                     )
-#                 print(count, mail)
-#                 time.sleep(random.randrange(10, 20)) # Пробуем обойти спам
+                count += 1
+                send_mail(
+                    'ООО Техносвар КС поздравляет Вас с новым годом!',
+                    message=html_content,
+                    from_email= 'market1@tehnosvar.ru',
+                    recipient_list= [f'{mail}',],
+                    fail_silently=False,
+                    html_message=html_content
+                    )
+                print(count, mail)
+                time.sleep(random.randrange(10, 20)) # Пробуем обойти спам
 
-#         return Response('Письмо отправлено')
+        return Response('Письмо отправлено')
 
 
 
-# @login_required
-# def send_payment_email(request, uuid):
-#     """ Отправка письма об оплате """
-#     try:
-#         order = CustomerModel.objects.get(uuid=uuid)
-#         products = OrderedProductModel.objects.filter(customer=order)
-#         html = "<html><body><h1>Письмо успешно отправлено</h1></br><a href='/a/orders/customermodel/'>Вернуться в админ панель</a></body></html>"
-#         html_content = render_to_string('permission_payment.html', {
-#                 'order_number': order.order_number,
-#                 'seller_comm': order.seller_comm,
-#                 'delivery': order.delivery,
-#                 'delivery_summ': order.delivery_summ,
-#                 'payment_link': f'{ settings.GLSVAR_FRONT }/payment?order={ order.order_number }',
-#                 'total': order.total,
-#                 'client_product': products,
-#         })
+@login_required
+def send_payment_email(request, uuid):
+    """ Отправка письма об оплате """
+    try:
+        order = CustomerModel.objects.get(uuid=uuid)
+        products = OrderedProductModel.objects.filter(customer=order)
+        html = "<html><body><h1>Письмо успешно отправлено</h1></br><a href='/a/orders/customermodel/'>Вернуться в админ панель</a></body></html>"
+        html_content = render_to_string('permission_payment.html', {
+                'order_number': order.order_number,
+                'seller_comm': order.seller_comm,
+                'delivery': order.delivery,
+                'delivery_summ': order.delivery_summ,
+                'payment_link': f'{ settings.GLSVAR_FRONT }/payment?order={ order.order_number }',
+                'total': order.total,
+                'client_product': products,
+        })
 
-#         send_mail(
-#             f'Вы можете оплатить ваш заказ {order.order_number}',
-#             message=html_content,
-#             from_email= 'shop@glsvar.ru',
-#             recipient_list= [f'{order.email}',],
-#             fail_silently=False,
-#             html_message=html_content
-#             )
-#         return HttpResponse(html)
-#     except CustomerModel.DoesNotExist:
-#         return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+        send_mail(
+            f'Вы можете оплатить ваш заказ {order.order_number}',
+            message=html_content,
+            from_email= 'shop@glsvar.ru',
+            recipient_list= [f'{order.email}',],
+            fail_silently=False,
+            html_message=html_content
+            )
+        return HttpResponse(html)
+    except CustomerModel.DoesNotExist:
+        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
 
 
 # def mail_template(request):
