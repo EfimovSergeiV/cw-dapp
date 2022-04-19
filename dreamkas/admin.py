6dd2e45daf7112840f84b6daeab788b08af83e61
anon <anon@debian-BULLSEYE-live-builder-AMD64>
2022-04-19 14:52:09 +0300

    bugfix

--- dreamkas/admin.py
+++ dreamkas/admin.py
@@ -1,64 +1,64 @@
-import json
-from pygments import highlight
-from pygments.lexers import JsonLexer
-from pygments.formatters import HtmlFormatter
+# import json
+# from pygments import highlight
+# from pygments.lexers import JsonLexer
+# from pygments.formatters import HtmlFormatter
 
-from csv import list_dialects
-from django.contrib import admin
-from django.utils.safestring import mark_safe
-from dreamkas.models import  DreamkasProductModel, ReceiptsStatusModel   # DreamkasPriceModel,
-
-
-# class PriceInlineAdmin(admin.TabularInline):
-#     model = DreamkasPriceModel
-#     extra = 0
+# from csv import list_dialects
+# from django.contrib import admin
+# from django.utils.safestring import mark_safe
+# from dreamkas.models import  DreamkasProductModel, ReceiptsStatusModel   # DreamkasPriceModel,
 
 
-class ProductAdmin(admin.ModelAdmin):
-    """ Форматирование товара """
-    list_display = ('association', 'name', 'id', 'createdAt','updatedAt',)
-    list_display_links = ('id', 'association', 'name')
-    fieldsets = (
-        ("", {'fields': (
-                'isMarked', 
-                ('name', 'association',),
-                ('id',),
-                ('type', 'quantity',),
-                ('tax',),
-                ('createdAt', 'updatedAt',),
-            )},
-        ),
-    )
-    # inlines = (PriceInlineAdmin, )
-    # list_editable = ('association', 'name',)
-    readonly_fields = ('createdAt', 'updatedAt',)
-    search_fields = ('id', 'association', 'name',)
+# # class PriceInlineAdmin(admin.TabularInline):
+# #     model = DreamkasPriceModel
+# #     extra = 0
 
 
-class ReceiptsStatusAdmin(admin.ModelAdmin):
-    """ Статусы фискализации чеков """
-
-    def get_check(self, obj):
-        """ Представление чека """
-        data = obj.cash_receipt
-        json_data = json.dumps(data, sort_keys=True, indent=2, ensure_ascii=False)
-        formatter = HtmlFormatter(style='colorful')
-        response = highlight(json_data, JsonLexer(), formatter)
-        style = "<style>" + formatter.get_style_defs() + "</style><br>"
-        return mark_safe(style + response)
-
-    get_check.short_description = 'JSON data'
-
-    readonly_fields = ('get_check', )
-    list_display = ('id', 'externalId', 'createdAt', 'status',)
-    list_display_links = ('id', 'externalId',)
-    fieldsets = (
-        ("Данные фискализации", {'fields': (('id', 'externalId',), ('createdAt', 'status',),)}),
-        ("Данные чека", {'fields': (('get_check',),)}),
-    )
-    # readonly_fields = ('id', 'externalId', 'createdAt', 'status',)
-    search_fields = ('id', 'externalId',)
+# class ProductAdmin(admin.ModelAdmin):
+#     """ Форматирование товара """
+#     list_display = ('association', 'name', 'id', 'createdAt','updatedAt',)
+#     list_display_links = ('id', 'association', 'name')
+#     fieldsets = (
+#         ("", {'fields': (
+#                 'isMarked', 
+#                 ('name', 'association',),
+#                 ('id',),
+#                 ('type', 'quantity',),
+#                 ('tax',),
+#                 ('createdAt', 'updatedAt',),
+#             )},
+#         ),
+#     )
+#     # inlines = (PriceInlineAdmin, )
+#     # list_editable = ('association', 'name',)
+#     readonly_fields = ('createdAt', 'updatedAt',)
+#     search_fields = ('id', 'association', 'name',)
 
 
-admin.site.register(DreamkasProductModel, ProductAdmin)
-admin.site.register(ReceiptsStatusModel, ReceiptsStatusAdmin)
+# class ReceiptsStatusAdmin(admin.ModelAdmin):
+#     """ Статусы фискализации чеков """
+
+#     def get_check(self, obj):
+#         """ Представление чека """
+#         data = obj.cash_receipt
+#         json_data = json.dumps(data, sort_keys=True, indent=2, ensure_ascii=False)
+#         formatter = HtmlFormatter(style='colorful')
+#         response = highlight(json_data, JsonLexer(), formatter)
+#         style = "<style>" + formatter.get_style_defs() + "</style><br>"
+#         return mark_safe(style + response)
+
+#     get_check.short_description = 'JSON data'
+
+#     readonly_fields = ('get_check', )
+#     list_display = ('id', 'externalId', 'createdAt', 'status',)
+#     list_display_links = ('id', 'externalId',)
+#     fieldsets = (
+#         ("Данные фискализации", {'fields': (('id', 'externalId',), ('createdAt', 'status',),)}),
+#         ("Данные чека", {'fields': (('get_check',),)}),
+#     )
+#     # readonly_fields = ('id', 'externalId', 'createdAt', 'status',)
+#     search_fields = ('id', 'externalId',)
+
+
+# admin.site.register(DreamkasProductModel, ProductAdmin)
+# admin.site.register(ReceiptsStatusModel, ReceiptsStatusAdmin)
--- dreamkas/serializers.py
+++ dreamkas/serializers.py
@@ -1,39 +1,39 @@
-from rest_framework import serializers
-from dreamkas.models import DreamkasProductModel, ReceiptsStatusModel
+# from rest_framework import serializers
+# from dreamkas.models import DreamkasProductModel, ReceiptsStatusModel
 
 
-# class DreamkasPriceSerializer(serializers.ModelSerializer):
-#     """ Серииализатор товаров """
+# # class DreamkasPriceSerializer(serializers.ModelSerializer):
+# #     """ Серииализатор товаров """
+
+# #     class Meta:
+# #         model = DreamkasPriceModel
+# #         fields = ('deviceId', 'value',)
+
+
+# class DreamkasProductSerializer(serializers.ModelSerializer):
+#     """ Сериализатор товаров """
+
+#     # prices = DreamkasPriceSerializer(many=True)
 
 #     class Meta:
-#         model = DreamkasPriceModel
-#         fields = ('deviceId', 'value',)
+#         model = DreamkasProductModel
+#         fields = (
+#             'id',
+#             'name',
+#             'type',
+#             'quantity',
+#             # 'createdAt',
+#             # 'updatedAt',
+#             'price',
+#             'tax',
+#             'isMarked',
+#             'prices',
+#         )
 
 
-class DreamkasProductSerializer(serializers.ModelSerializer):
-    """ Сериализатор товаров """
+# class ReceiptsStatusSerializer(serializers.ModelSerializer):
+#     """ Сериализатор товаров """
 
-    # prices = DreamkasPriceSerializer(many=True)
-
-    class Meta:
-        model = DreamkasProductModel
-        fields = (
-            'id',
-            'name',
-            'type',
-            'quantity',
-            # 'createdAt',
-            # 'updatedAt',
-            'price',
-            'tax',
-            'isMarked',
-            'prices',
-        )
-
-
-class ReceiptsStatusSerializer(serializers.ModelSerializer):
-    """ Сериализатор товаров """
-
-    class Meta:
-        model = ReceiptsStatusModel
-        fields = ('id', 'externalId', 'createdAt', 'status',)
+#     class Meta:
+#         model = ReceiptsStatusModel
+#         fields = ('id', 'externalId', 'createdAt', 'status',)
