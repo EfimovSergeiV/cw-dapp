6d2db22074cf53600ebe00b46f5461e70aef003a
anon <efimovsergeiv@gmail.com>
2022-12-27 16:04:40 +0300

    add:

--- catalog/management/commands/price_migrate.py
+++ catalog/management/commands/price_migrate.py
@@ -2,11 +2,14 @@
 
 Мигрирование цен товаров в единое поле
 
+1. Экспорт цен в таблицу с разбором стоимости валюты и общей цены
+
 """
 
 from django.core.management.base import BaseCommand, CommandError
 from catalog.models import *
 import csv, requests, json
+import xlsxwriter
 
 
 class Command(BaseCommand):
@@ -18,6 +21,8 @@ class Command(BaseCommand):
 
 
 count = 0
+
+category = CategoryModel.objects.all()
 queryset = ProductModel.objects.filter(activated=True)
 prices_qs = PriceModel.objects.all()
 
@@ -28,20 +33,58 @@ currency = {
     "RUB": 1
 }
 
+expenses = []
+
+for qs_ct in category:
+    print(f'{ qs_ct.id } { qs_ct.name }')
+    expenses.append([qs_ct.id, qs_ct.name, 'category',])
+
+    for product in queryset.filter(category=qs_ct.id):
+        count += 1
+
+        if product.only_price_status:
+            price = int(product.only_price * currency[product.currency])
+            print(f"\n{ count }\nid: { product.id }\nname: { product.name }\nO: { price } TRUE")  #{ product.category_id }
+
+            product = [product.id, product.name, price]
+        else:
+            price_qs = prices_qs.filter(product = product.id)
+            price = int(price_qs[0].price * currency[price_qs[0].currency])
+            print(f"\n{ count }\nid: { product.id }\nname: { product.name }\nO: {price}")
+
+            product = [product.id, product.name, price]
+
+        expenses.append(product)
 
 
+workbook = xlsxwriter.Workbook('/home/anon/backup/prods.xlsx')
+worksheet = workbook.add_worksheet()
 
+bold = workbook.add_format({'bold': True})
+name = workbook.add_format()
 
-for product in queryset:
-    count += 1
-    
-    if product.only_price_status:
-        price = int(product.only_price * currency[product.currency])
-        print(f"\n{ count }\nid: { product.id }\nname: { product.name }\nO: { price } TRUE")  #{ product.category_id }
+worksheet.set_column(0, 0, 10)
+worksheet.set_column(1, 1, 120)
+worksheet.set_column(2, 2, 10)
+
+worksheet.write('A1', 'id', bold)
+worksheet.write('B1', 'Название', bold)
+worksheet.write('C1', 'Стоимость', bold)
+
+row = 1
+col = 0
+
+for id, name, price in (expenses):
+    if price == 'category':
+        row += 2
+
+        worksheet.write(f'A{row}', id, bold)
+        worksheet.write(f'B{row}', name, bold)
 
     else:
-        price_qs = prices_qs.filter(product = product.id)
+        worksheet.write(row, col, id, )
+        worksheet.write(row, col + 1, name)
+        worksheet.write(row, col + 2, price)
+    row += 1
 
-        price = int(price_qs[0].price * currency[price_qs[0].currency])
-
-        print(f"\n{ count }\nid: { product.id }\nname: { product.name }\nO: {price}")
+workbook.close()
