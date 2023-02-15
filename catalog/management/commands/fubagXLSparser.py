a9aa08ea37e5f9315af03cc564c8a73b48381b1d
anon <efimovsergeiv@gmail.com>
2023-02-15 16:37:09 +0300

    add:

--- catalog/management/commands/fubagXLSparser.py
+++ catalog/management/commands/fubagXLSparser.py
@@ -52,31 +52,86 @@ from elasticsearch_dsl import Search
 from elasticsearch import Elasticsearch
 
 client = Elasticsearch()
-
 s = Search(using=client)
-
 document_class = ProductDocument
 
 
-search_query = 'Источник для сварки под флюсом SW 1000 (38 672) + трактор сварочный TW 1000 (38 673) '
+search_query = 'Источник для сварки под флюсом SW 1000 (38 672) + трактор сварочный TW 1000 (38 673) + набор соединительных кабелей (38847)'
+
+name = 'FUBAG Источник для сварки под флюсом SW 1250'
+# name = 'FUBAG сварочный трактор TW 1000'
+# name = 'FUBAG источник SW 1000'
+# name = 'Автомат для сварки MZ 1000'
+# name = 'FUBAG сварочный трактор TW 1250'
+# name = 'Электрогенератор EUROLUX G4000A 64/1/38'
+# name = 'IDEALARC DC-1000'
+# name = 'VERSOTRAC EWT 1000'
+# name = 'Преобразователь CONVERTER 1000'
+# name = 'Блок подачи воздуха PAPR 1000 mm Hose'
+
+
 
 s = Search().using(client).query("match", name=search_query,)
 
-query = Q('multi_match', query=search_query,
-        fields=[
-            'vcode',
-            'name',
-            'keywords',
-        ], fuzziness='auto')
-
+query = Q('match', name=search_query)
 search = document_class.search().query(query)
 response = search.execute()
 
 
+
+def word_list(text):
+    """ Чистит текст запроса и возвращает список слов из запроса """
+    return text.replace('(', '').replace(')', '').replace('+', '').replace('-', '').split()
+
+
+
+print(f'\n\nsearch query: { word_list(search_query) }\n')
+
+word_list_query = word_list(search_query)
+
+for product in response:
+    
+    word_list_product = word_list(product.name)
+    lenght_word_list_product = len(word_list_product)
+
+    like = 0
+
+    for word in word_list_product:
+        if word in word_list_query:
+            like += 1
+
+    print(f'like: { like }/{ lenght_word_list_product },  name: { word_list(product.name) }')
+
+
+"""
+like: 6/8,  name: ['FUBAG', 'Источник', 'для', 'сварки', 'под', 'флюсом', 'SW', '1250']
+like: 4/5,  name: ['FUBAG', 'сварочный', 'трактор', 'TW', '1000']
+like: 2/4,  name: ['FUBAG', 'источник', 'SW', '1000']
+like: 3/5,  name: ['Автомат', 'для', 'сварки', 'MZ', '1000']
+like: 3/5,  name: ['FUBAG', 'сварочный', 'трактор', 'TW', '1250']
+like: 0/4,  name: ['Электрогенератор', 'EUROLUX', 'G4000A', '64/1/38']
+like: 0/2,  name: ['IDEALARC', 'DC1000']
+like: 1/3,  name: ['VERSOTRAC', 'EWT', '1000']
+like: 1/3,  name: ['Преобразователь', 'CONVERTER', '1000']
+like: 1/7,  name: ['Блок', 'подачи', 'воздуха', 'PAPR', '1000', 'mm', 'Hose']
+"""
+
+
 print(f'\nFound { len(response) }/{ response.hits.total.value } hit(s) for query: "{ search_query }"\n')
 
-for product in  response:
-    print(product.name)
+
+
+
+# for word in word_list(name):
+#     print(word)
+
+# print('\n')
+
+# for word in word_list(search_query):
+#     print(word)
+
+
+# print(clearing_request(search_query))
 
 
 
