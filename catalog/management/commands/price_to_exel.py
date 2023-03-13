6dd3a93694266f55b174ea053e53cdb4b6af62c1
Sergei Efimov <efimovsergeiv@gmail.com>
2023-03-13 13:26:00 +0300

    add:

--- catalog/management/commands/price_to_exel.py
+++ catalog/management/commands/price_to_exel.py
@@ -29,76 +29,54 @@ status_translator = {
 
 
 print(f'{BASE_DIR}/files/')
+expenses = []
 
-# for category in [4, 14, 35, 34, 31, 28, 25 , 18, 32, 29, 23, 33, 30, 27]:
-#     ct_qs = cts_qs.get(id=category)
+for category in [4, 14, 35, 34, 31, 28, 25 , 18, 32, 29, 23, 33, 30, 27]:
+    ct_qs = cts_qs.get(id=category)
 
-#     print(f'\n{ct_qs.id} {ct_qs.name}')
+    print(f'\n{ct_qs.id} {ct_qs.name}')
     
-#     for ct_prods_qs in prods_qs.filter(category=category):
-#         status = status_translator[ct_prods_qs.status]
-#         print(f'{ ct_prods_qs.id }.\t{ ct_prods_qs.only_price } RUB\t{ status }\t{ ct_prods_qs.name }')
+    for ct_prods_qs in prods_qs.filter(category=category):
+        status = status_translator[ct_prods_qs.status]
+
+        product = [
+            ct_prods_qs.id, 
+            ct_prods_qs.name, 
+            ct_prods_qs.only_price
+        ]
+
+        expenses.append(product)
+        print(f'{ ct_prods_qs.id }.\t{ ct_prods_qs.only_price } RUB\t{ status }\t{ ct_prods_qs.name }')
 
 
+workbook = xlsxwriter.Workbook(f'{BASE_DIR}/files/xlsx/prods-ct-4.xlsx')
+worksheet = workbook.add_worksheet()
 
+bold = workbook.add_format({'bold': True})
+name = workbook.add_format()
 
+worksheet.set_column(0, 0, 10)
+worksheet.set_column(1, 1, 120)
+worksheet.set_column(2, 2, 10)
 
-# expenses = []
+worksheet.write('A1', 'id', bold)
+worksheet.write('B1', 'Название', bold)
+worksheet.write('C1', 'Стоимость', bold)
 
+row = 1
+col = 0
 
+for id, name, price in (expenses):
+    if price == 'category':
+        row += 2
 
-# for qs_ct in category:
-#     print(f'{ qs_ct.id } { qs_ct.name }')
-#     expenses.append([qs_ct.id, qs_ct.name, 'category',])
+        worksheet.write(f'A{row}', id, bold)
+        worksheet.write(f'B{row}', name, bold)
 
-#     for product in queryset.filter(category=qs_ct.id):
-#         """ + Добавить к парсеру наличие товаров """
-        
-#         count += 1
+    else:
+        worksheet.write(row, col, id, )
+        worksheet.write(row, col + 1, name)
+        worksheet.write(row, col + 2, price)
+    row += 1
 
-#         if product.only_price_status:
-#             price = int(product.only_price * currency[product.currency])
-#             print(f"\n{ count }\nid: { product.id }\nname: { product.name }\nO: { price } TRUE")  #{ product.category_id }
-
-#             product = [product.id, product.name, price]
-#         else:
-#             price_qs = prices_qs.filter(product = product.id)
-#             price = int(price_qs[0].price * currency[price_qs[0].currency])
-#             print(f"\n{ count }\nid: { product.id }\nname: { product.name }\nO: {price}")
-
-#             product = [product.id, product.name, price]
-
-#         expenses.append(product)
-
-
-# workbook = xlsxwriter.Workbook('/home/anon/backup/prods1.xlsx')   # Сменить путь для сервера
-# worksheet = workbook.add_worksheet()
-
-# bold = workbook.add_format({'bold': True})
-# name = workbook.add_format()
-
-# worksheet.set_column(0, 0, 10)
-# worksheet.set_column(1, 1, 120)
-# worksheet.set_column(2, 2, 10)
-
-# worksheet.write('A1', 'id', bold)
-# worksheet.write('B1', 'Название', bold)
-# worksheet.write('C1', 'Стоимость', bold)
-
-# row = 1
-# col = 0
-
-# for id, name, price in (expenses):
-#     if price == 'category':
-#         row += 2
-
-#         worksheet.write(f'A{row}', id, bold)
-#         worksheet.write(f'B{row}', name, bold)
-
-#     else:
-#         worksheet.write(row, col, id, )
-#         worksheet.write(row, col + 1, name)
-#         worksheet.write(row, col + 2, price)
-#     row += 1
-
-# workbook.close()
+workbook.close()
