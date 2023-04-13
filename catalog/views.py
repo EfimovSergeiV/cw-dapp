ba76c99659886a3ee7ab7e00d59c04f8548dd834
Sergei Efimov <efimovsergeiv@gmail.com>
2023-04-13 13:26:39 +0300

    add:

--- catalog/views.py
+++ catalog/views.py
@@ -308,7 +308,7 @@ class OneRandomProductView(APIView):
                     all_categories += [third_child.id for third_child in third_child_qs.get_children()]
 
                 prods.append(self.queryset.filter(category_id__in=all_categories).order_by("?")[0])
-            serializer = self.serializer_class(prods, many=True, context={'request': request})
+            serializer = self.serializer_class(prods[0:4], many=True, context={'request': request})
             
             return Response(serializer.data)
         
