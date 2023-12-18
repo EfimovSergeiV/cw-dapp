3e3f112dc6babec9c9fe0fd009c0630ab03e35fb
Сергей Ефимов <efimovsergeiv@gmail.com>
2023-12-18 14:01:38 +0300

    add:

--- catalog/management/commands/yaml-generator.py
+++ catalog/management/commands/yaml-generator.py
@@ -10,93 +10,74 @@ class Command(BaseCommand):
         pass
 
 
-products = ProductModel.objects.filter(activated=True)
+products = ProductModel.objects.filter(id__in = [ 312, 313, 314, 315, 316, 317, 318, 319 ])
 
-offer = f"""
-<offer id="9012">
-    <name>Мороженица Brand 3811</name>
-    <vendor>Brand</vendor>
-    <vendorCode>A1234567B</vendorCode>
-    <url>http://best.seller.ru/product_page.asp?pid=12345</url>
-    <price>8990</price>
-    <oldprice>9990</oldprice>
-    <enable_auto_discounts>true</enable_auto_discounts>
-    <currencyId>RUR</currencyId>
-    <categoryId>101</categoryId>
-    <picture>http://best.seller.ru/img/model_12345.jpg</picture>
-    <description>
-        <![CDATA[          
-            <p>Это прибор, который придётся по вкусу всем любителям десертов и сладостей</p>
-            <p>С его помощью вы сможете делать вкусное домашнее мороженое из натуральных ингредиентов.</p>
-        ]]>
-    </description>                
-    <sales_notes>Необходима предоплата.</sales_notes>
-    <manufacturer_warranty>true</manufacturer_warranty>
-    <barcode>4601546021298</barcode>
-    <param name="Цвет">белый</param>
-    <weight>3.6</weight>
-    <dimensions>20.1/20.551/22.5</dimensions>
-    <condition type="preowned">
-        <quality>excellent</quality>
-    </condition>
-</offer>
-"""
 
-template = f"""
-<?xml version="1.0" encoding="UTF-8"?>
-<yml_catalog date="2020-11-22T14:37:38+03:00">
-    <shop>
-        <name>BestSeller</name>
-        <company>Tne Best inc.</company>
-        <url>http://best.seller.ru</url>
-        <platform>uCoz</platform>
-        <categories>
-            <category id="1">Бытовая техника</category>
-            <category id="10" parentId="1">Мелкая техника для кухни</category>
-            <category id="101" parentId="10">Сэндвичницы и приборы для выпечки</category>
-        </categories>
-        <delivery-options>
-            <option cost="200" days="1"/>
-        </delivery-options>
-        <pickup-options>
-            <option cost="200" days="1"/>
-        </pickup-options>
-        <offers>
+offers = ''
+offer_template = """\n      <offer id="{id}">
+        <name>{name}</name>
+        <vendor>{brand}</vendor>
+        <vendorCode>{vcode}</vendorCode>
+        <url>https://www.glsvar.ru/product/{id}</url>
+        <price>{price}</price>
+        <currencyId>RUB</currencyId>
+        <categoryId>{ct_id}</categoryId>
+        <picture>https://api.glsvar.ru/files/{picture}</picture>
+        <description>
+          <![CDATA[          
+            <p>{description}</p>
+          ]]>
+        </description>
+      </offer>\n"""
 
-            <offer id="9012">
-                <name>Мороженица Brand 3811</name>
-                <vendor>Brand</vendor>
-                <vendorCode>A1234567B</vendorCode>
-                <url>http://best.seller.ru/product_page.asp?pid=12345</url>
-                <price>8990</price>
-                <oldprice>9990</oldprice>
-                <enable_auto_discounts>true</enable_auto_discounts>
-                <currencyId>RUR</currencyId>
-                <categoryId>101</categoryId>
-                <picture>http://best.seller.ru/img/model_12345.jpg</picture>
-                <description>
-                    <![CDATA[          
-                        <p>Это прибор, который придётся по вкусу всем любителям десертов и сладостей</p>
-                        <p>С его помощью вы сможете делать вкусное домашнее мороженое из натуральных ингредиентов.</p>
-                    ]]>
-                </description>                
-                <sales_notes>Необходима предоплата.</sales_notes>
-                <manufacturer_warranty>true</manufacturer_warranty>
-                <barcode>4601546021298</barcode>
-                <param name="Цвет">белый</param>
-                <weight>3.6</weight>
-                <dimensions>20.1/20.551/22.5</dimensions>
-                <condition type="preowned">
-                    <quality>excellent</quality>
-                </condition>
-            </offer>
 
-        </offers>
-    </shop>
+
+for product in products:
+    offer = offer_template.format(
+        name = product.name,
+        brand = product.brand,
+        vcode = product.vcode,
+        id = product.id,
+        price = int(product.only_price),
+        ct_id = product.category_id,
+        picture = product.preview_image,
+        description = product.description,
+    )
+    offers += offer
+    print(offer)
+
+
+# print(offers)
+
+
+yml_template = """<?xml version="1.0" encoding="UTF-8"?>
+  <yml_catalog date="2023-12-15T12:00:00+03:00">
+  <shop>
+    <name>Главный Сварщик</name>
+    <company>Главный Сварщик</company>
+    <url>https://glsvar.ru</url>
+    <email>zakaz@glsvar.ru</email>
+    <currencies>
+      <currency id="RUB" rate="1"></currency>
+    </currencies>
+    <categories>
+      <category id="31">Сварочные электроды</category>
+    </categories>
+    <offers>
+{offers}
+    </offers>
+  </shop>
 </yml_catalog>
 """
 
-print(template)
 
-for product in products[0:2]:
-    print(product.name)
+
+with open(f'./esab-elektrods.xml', 'w' ) as file:
+    file.write(
+        yml_template.format(
+            offers = offers
+        )
+    )
+
+for product in products:
+    print(f'{int(product.only_price)} руб. - {product.name}')
